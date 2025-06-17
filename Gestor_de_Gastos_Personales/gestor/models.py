9313af2765a348a3e7ee91d039d5ba9efe91594f
PfAlvarado05 <pedroticona8@gmail.com>
2025-06-16 22:16:10 -0400

    Añadiendo el models.py final
    Añado el models final del proyecto

--- Gestor_de_Gastos_Personales/gestor/models.py
+++ Gestor_de_Gastos_Personales/gestor/models.py
@@ -14,14 +14,31 @@ class Presupuesto(models.Model):
     limite = models.FloatField()
 
     def __str__(self):
-        return f"{self.usuario} - {self.categoria} - Límite: {self.limite}"
+        return f"{self.usuario.username} - {self.categoria.nombre} - Límite: {self.limite}"
 
 class Transaccion(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     categoria = models.ForeignKey(CategoriaGasto, on_delete=models.CASCADE)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha = models.DateField(auto_now_add=True)
+    fecha = models.DateField()
     descripcion = models.TextField(blank=True)
 
     def __str__(self):
-        return f'{self.categoria} - {self.monto}'
+        return f'{self.usuario.username} - {self.categoria.nombre} - {self.monto}'
+
+class Ingreso(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField()
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.usuario.username} - Ingreso de {self.monto} en {self.fecha}'
+
+class IngresoMensual(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    mes = models.DateField()
+
+    def __str__(self):
+        return f"{self.usuario.username} - {self.mes.strftime('%B %Y')} - {self.monto}"
